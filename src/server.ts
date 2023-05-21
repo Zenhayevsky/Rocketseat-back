4324e4d821c74ebb9ed0fa426bd3b23dfa9af365
Zenhayevsky <74720973+Zenhayevsky@users.noreply.github.com>
2023-05-21 05:33:37 -0300

    log + rotas +memories
    log de query
    sepraracao de arquivos de rotas
    nova rota memories

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
-
-app.get('/users', async () => {
-  const users = await prisma.users.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
